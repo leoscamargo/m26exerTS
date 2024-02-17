3c07bdcce3004ebf4cad3b3bdd123dfcb26bc2e1
Leonardo <leogne98@hotmail.com>
2024-02-16 22:39:50 -0300

    fist commit

---
+++ src/functions.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+const multiplicado = multiplicacao(2, 4);
+console.log(multiplicado);
---
+++ src/functions.ts
@@ -0,0 +1,7 @@
+function multiplicacao(numero1: number, numero2: number): number{
+    return numero1 * numero2;
+}
+
+const multiplicado: number = multiplicacao(2,4);
+
+console.log(multiplicado);
